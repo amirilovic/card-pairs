4331c191222977d4e3a14cf7da6ab44505134b47
Aleksandar Djindjic <djindjic@gmail.com>
2016-07-31 22:39:47 +0200

    desk
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- src/domain/game.js
+++ src/domain/game.js
@@ -1,25 +1,25 @@
-import Board from './board';
+import Desk from './desk';
 import {CardStatus} from './card';
 
 export default class {
     constructor($timeout, dimension) {
         this.$timeout = $timeout;
         this._attemptsCount = 0;
-        this._board = new Board((dimension * dimension) / 2);
-        this._rows = this._createRows(dimension, this._board.cards);
+        this._desk = new Desk((dimension * dimension) / 2);
+        this._rows = this._createRows(dimension, this._desk.cards);
     }
     get attemptsCount() { return this._attemptsCount; }
     get rows() { return this._rows; }
     flip(card) {
-        if(card.status === CardStatus.CLOSED && this._board.openedCards.length < 2) {
+        if(card.status === CardStatus.CLOSED && this._desk.openedCards.length < 2) {
             card.open();
-            if(this._board.openedCards.length === 2) {
+            if(this._desk.openedCards.length === 2) {
                 this._attemptsCount++;
-                if(this._board.openedCards[0].label === this._board.openedCards[1].label) {
-                    this._board.openedCards.forEach((card) => card.resolve());
+                if(this._desk.openedCards[0].label === this._desk.openedCards[1].label) {
+                    this._desk.openedCards.forEach((card) => card.resolve());
                 } else {
                     this.$timeout(() => {
-                        this._board.openedCards.forEach((card) => card.close());
+                        this._desk.openedCards.forEach((card) => card.close());
                     }, 1000);
                 }
             }
